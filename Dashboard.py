947122f91e22c94a45c08886cbb837f81cd94283
Carlos Adrian Salguero Torres <adriansalguero.t@gmail.com>
2025-09-21 02:43:08 -0500

    Remove language parameter from month_name method

--- Dashboard.py
+++ Dashboard.py
@@ -81,7 +81,7 @@ fact_ciudades = datos.drop_duplicates(subset=['Lugar de Compra'])[['Lugar de Com
 facturacion_mensual = datos.set_index('Fecha de Compra').groupby(pd.Grouper(freq='ME'))['Precio'].sum().reset_index()
 
 facturacion_mensual['Año'] = facturacion_mensual['Fecha de Compra'].dt.year
-facturacion_mensual['Mes'] = facturacion_mensual['Fecha de Compra'].dt.month_name('es')
+facturacion_mensual['Mes'] = facturacion_mensual['Fecha de Compra'].dt.month_name()
 
 facturacion_cat = datos.groupby('Categoría del Producto')[['Precio']].sum().sort_values('Precio', ascending=False)
 
@@ -132,7 +132,7 @@ cantidad_ventas.index.name = "Categoría del Producto"
 ## cantidad_ventas_mensual
 cantidad_ventas_mensual = datos.set_index('Fecha de Compra').groupby(pd.Grouper(freq='ME')).count().reset_index()
 cantidad_ventas_mensual['Año'] = cantidad_ventas_mensual['Fecha de Compra'].dt.year
-cantidad_ventas_mensual['Mes'] = cantidad_ventas_mensual['Fecha de Compra'].dt.month_name('es')
+cantidad_ventas_mensual['Mes'] = cantidad_ventas_mensual['Fecha de Compra'].dt.month_name()
 
 ## cantidad_ventas_ciudades
 cantidad_ventas_ciudades = datos.groupby('Lugar de Compra')[['Lugar de Compra']].count()
